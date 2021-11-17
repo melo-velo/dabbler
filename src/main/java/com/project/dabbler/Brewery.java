9bafcf883a7866da234b13055a23cd0116bb36a8
Martti Jaakola <JAAK8280@STTHOMAS.EDU>
2021-11-17 00:17:23 -0600

    Added main class called Dabbler and a Brewery class which will act as data model

---
+++ src/main/java/com/project/dabbler/Brewery.java
@@ -0,0 +1,4 @@
+package com.project.dabbler;
+
+public class Brewery {
+}
---
+++ src/main/java/com/project/dabbler/Dabbler.java
@@ -0,0 +1,13 @@
+package com.project.dabbler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dabbler {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dabbler.class, args);
+    }
+
+}
