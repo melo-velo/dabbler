29b54cb834a67c632ad422293301ea94c70f7c4d
Martti Jaakola <JAAK8280@STTHOMAS.EDU>
2021-11-24 01:38:01 -0600

    Added comments to clarify what certain declarations mean.

--- src/main/java/com/project/dabbler/brewery/Brewery.java
+++ src/main/java/com/project/dabbler/brewery/Brewery.java
@@ -10,13 +10,21 @@ import lombok.*;
 @AllArgsConstructor
 
 public class Brewery {
+
     private Long id;
+    // Name of Brewery
     private String name;
+    //Where the brewery is located
     private String location;
+    //What type of drink it is
     private Drink drink;
+    //What type of beer it is or other for non-alcoholic beverages
     private Type type;
+    //Name of beer
     private String title;
+    //Price of beer
     private int price;
+    //Website address
     private String url;
 
 }
