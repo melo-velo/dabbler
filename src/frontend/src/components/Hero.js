50cb211659bf7a9d49f62b9269f2f16b92adc926
melo-velo <jaak8280@stthomas.edu>
2021-12-05 20:11:21 -0700

    Changed hero text color and added some space with line break

--- src/frontend/src/components/Hero.js
+++ src/frontend/src/components/Hero.js
@@ -7,6 +7,7 @@ const Hero = () => {
             <img src={hero} className='hero-image' alt='Hero image'/>
             <div className='hero-text'>
                 <p className="hero-title">Dabbler</p>
+                <br/>
                 <p>Minnesota's most prestigious brewery rankings.</p>
                 <a href='#rankings'><button className='hero-button'>The List</button></a>
             </div>
