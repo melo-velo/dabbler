4d2a21c7a8863dd360385c020bd21a847704f98f
melo-velo <jaak8280@stthomas.edu>
2021-12-06 12:46:25 -0700

    Updated main class with properties, and added a junit test

--- src/main/java/com/project/dabbler/brewery/Brewery.java
+++ src/main/java/com/project/dabbler/brewery/Brewery.java
@@ -9,7 +9,7 @@ import java.net.URL;
 @Setter
 @EqualsAndHashCode
 @NoArgsConstructor
-@AllArgsConstructor
+//@AllArgsConstructor
 
 public class Brewery {
     private Long id;
@@ -27,4 +27,79 @@ public class Brewery {
     private int numberOfBeers;
     //Website address
     private String url;
+
+    public Brewery(Long id, String name, String location, Drink drink, Type type, String title, int numberOfBeers, String url) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.drink = drink;
+        this.type = type;
+        this.title = title;
+        this.numberOfBeers = numberOfBeers;
+        this.url = url;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setDrink(Drink drink) {
+        this.drink = drink;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setNumberOfBeers(int numberOfBeers) {
+        this.numberOfBeers = numberOfBeers;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public Drink getDrink() {
+        return drink;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getNumberOfBeers() {
+        return numberOfBeers;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
--- src/test/java/com/project/dabbler/DabblerTests.java
+++ src/test/java/com/project/dabbler/DabblerTests.java
@@ -1,13 +1,45 @@
 package com.project.dabbler;
 
+import com.project.dabbler.brewery.Brewery;
+import com.project.dabbler.brewery.BreweryService;
+import com.project.dabbler.brewery.Drink;
+import com.project.dabbler.brewery.Type;
+import lombok.val;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
+//JUnit test examples, all should pass before committing code to master
 class DabblerApplicationTests {
 
+    @Autowired
+    private BreweryService myTestBrewery;
+
     @Test
-    void contextLoads() {
+    //Test checks various parameters to see if controller functions return values
+    void checkIfBreweryIdExists() {
+        Brewery brewery = new Brewery(1L,
+                "Dabbler Brewing Co. ",
+                "Minneapolis",
+                Drink.BEER,
+                Type.PILSNER,
+                "Dabbler",
+                1,
+                "Dabbler.us-east-1.elasticbeanstalk.com");
+
+        //Given
+        var underTest = myTestBrewery.getBrewery(1L);
+
+        var testID = underTest.getId();
+
+        //When id is equivalent to 1, convert to string.
+        val testValue = testID.toString();
+
+        //Then check to see if test value is correct.
+        assertThat(testValue).isEqualTo("1");
     }
 
 }
