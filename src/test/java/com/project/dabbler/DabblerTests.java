5433cc326afc465b1010b5b2cbf3993c95b96373
melo-velo <jaak8280@stthomas.edu>
2021-12-06 13:01:05 -0700

    Added a few more JUnit tests for project

--- src/test/java/com/project/dabbler/DabblerTests.java
+++ src/test/java/com/project/dabbler/DabblerTests.java
@@ -22,7 +22,7 @@ class DabblerApplicationTests {
     //Test checks various parameters to see if controller functions return values
     void checkIfBreweryIdExists() {
         Brewery brewery = new Brewery(1L,
-                "Dabbler Brewing Co. ",
+                "Dabbler Brewing Co.",
                 "Minneapolis",
                 Drink.BEER,
                 Type.PILSNER,
@@ -40,6 +40,66 @@ class DabblerApplicationTests {
 
         //Then check to see if test value is correct.
         assertThat(testValue).isEqualTo("1");
+    }
+
+    @Test
+    //test uses real data instead of mock data
+    void checkIfNameReturnsCorrect() {
+        //Given
+        var underTest = myTestBrewery.getBrewery(1L);
+
+        var testName = underTest.getName();
+
+        //When id is equivalent to 1, convert to string.
+        val testValue = testName;
+
+        //Then check to see if test value is correct.
+        assertThat(testValue).isEqualTo("Bald Man");
+    }
+
+    @Test
+        //test switches params and checks if location correct
+    void checkIfLocationCorrect() {
+        //Given
+        var underTest = myTestBrewery.getBrewery(3L);
+
+        var location = underTest.getLocation();
+
+        //When id is equivalent to 1, convert to string.
+        val testValue = location;
+
+        //Then check to see if test value is correct.
+        assertThat(testValue).isEqualTo("Northeast Minneapolis");
+    }
+
+    @Test
+        //test switches params and checks if drink type correct, tests enums
+    void checkIfTypeCorrect() {
+        //Given
+        var underTest = myTestBrewery.getBrewery(3L);
+
+        var drink = underTest.getDrink();
+
+        //When id is equivalent to 1, convert to string.
+        val testValue = drink;
+
+        //Then check to see if test value is correct.
+        assertThat(testValue).isEqualTo(Drink.BEER);
+    }
+
+    @Test
+        //test switches params and checks if drink type correct, tests enums
+    void checkIfTitleCorrect() {
+        //Given
+        var underTest = myTestBrewery.getBrewery(10L);
+
+        var title = underTest.getTitle();
+
+        //When id is equivalent to 1, convert to string.
+        val testValue = title;
+
+        //Then check to see if test value is correct.
+        assertThat(testValue).isEqualTo("Rain Drops");
     }
 
 }
